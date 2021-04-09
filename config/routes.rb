2cb4fcc2a8568365faa6a4964f019df4409628e1
nakamurammm <mr.mar2.sor@gmail.com>
2021-04-09 05:17:42 +0000

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/show'
+  get 'users/index'
+  get 'users/edit'
+  get 'users/update'
+  devise_for :users
 end
