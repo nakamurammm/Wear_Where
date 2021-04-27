647865c9707a2be4a2a10e3bd0d17576342e64f8
nakamurammm <mr.mar2.sor@gmail.com>
2021-04-27 01:52:05 +0000

    微修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  unless Rails.env.production?
   config.web_console.whitelisted_ips = '210.139.236.150' #追記
+  end
 end
--- db/migrate/20210422105403_remove_clothes_image_id_from_post_images.rb
+++
@@ -1,5 +0,0 @@
-class RemoveClothesImageIdFromPostImages < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :post_images, :clothes_image_id, :integer
-  end
-end
