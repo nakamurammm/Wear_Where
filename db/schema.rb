92973532e6fd043bd20354292aac884ea18987b7
nakamurammm <mr.mar2.sor@gmail.com>
2021-04-27 02:07:29 +0000

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2021_04_23_063033) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
