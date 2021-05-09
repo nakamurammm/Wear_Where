71945888533958448239715ea471a7a0b20b97db
nakamurammm <mr.mar2.sor@gmail.com>
2021-05-09 04:39:44 +0000

    edit-api

--- app/controllers/dresses_controller.rb
+++ app/controllers/dresses_controller.rb
@@ -27,24 +27,11 @@ class DressesController < ApplicationController
 
   def index
     @dresses = Dress.all
-    @dresses1 = Dress.where(genre_id: 1)
-    @dresses2 = Dress.where(genre_id: 2)
-    @dresses3 = Dress.where(genre_id: 3)
-    @dresses4 = Dress.where(genre_id: 4)
-    @dresses5 = Dress.where(genre_id: 5)
-    @dresses6 = Dress.where(genre_id: 6)
-    @dresses7 = Dress.where(genre_id: 7)
-    @dresses8 = Dress.where(genre_id: 8)
-    @dresses9 = Dress.where(genre_id: 9)
-    @dresses10 = Dress.where(genre_id: 10)
-    @dresses11 = Dress.where(genre_id: 11)
-    @dresses12 = Dress.where(genre_id: 12)
-
+    @genres = Genre.all
   end
 
   def show
     @dress = Dress.find(params[:id])
-
   end
 
   def edit
@@ -56,11 +43,16 @@ class DressesController < ApplicationController
 
   def update
     @dress = Dress.find(params[:id])
-
-    @dress.update(dress_params)
-
-    redirect_to dress_path(@dress.id)
+    if @dress.update(dress_params)
+      @dress.tags.destroy_all
+      tags = Vision.get_image_data(@dress.image)
+        tags.each do |tag|
+          @dress.tags.create(name: tag)
+        end
+      redirect_to dress_path(@dress.id)
+    end
   end
+
   def destroy
     @dress = Dress.find(params[:id])
     @dress.destroy
